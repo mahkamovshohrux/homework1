f142718363ca2fe3dbadd6cb87a5b9e5106f2522
mahkamovshohrux <mahkamovshohrux1909@gmail.com>
2023-10-05 08:59:28 +0500

    home

---
+++ app0/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import SellerModel1
+
+class SellerSerializer1(serializers.ModelSerializer):
+    class Meta:
+        model = SellerModel1
+        fields = ("seller_txt","pub_date")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
